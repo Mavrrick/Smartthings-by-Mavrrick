cbcca730c9bc89f492e48083a0a16da166e85613
CRAIG KING <mavrrick58@gmail.com>
2018-04-27 22:28:50 -0400

    ADT Tools

---
+++ smartapps/mavrrick/adt-alarm-action.src/adt-alarm-action.groovy
@@ -0,0 +1,254 @@
+/**
+ *  ADT Alarm Action
+ *
+ *  Copyright 2018 CRAIG KING
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "ADT Alarm Action",
+    namespace: "Mavrrick",
+    author: "CRAIG KING",
+    description: "Smartthing ADT tools for additional functions ",
+    category: "Safety & Security",
+    parent: "Mavrrick:ADT Tools",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+/* 
+* Initial release v1.0.0
+* Trigger action based on ADT Alarm. This is intial release of child app
+*/
+preferences {
+	    section("Use these devices when ADT Alarm is triggered"){
+		input "contact", "capability.contactSensor", title: "Look for ADT Activity on these contact sesors", required: false, multiple: true
+	    input "water", "capability.waterSensor", title: "Look for ADT Activity on these water sesors", required: false, multiple: true
+		input "motion", "capability.motionSensor", title: "Look for ADT Activity on these motion sesors", required: false, multiple: true
+		input "smoke", "capability.smokeDetector", title: "Smoke Detected", required: false, multiple: true
+		}
+        section("Action to trigger when ADT Alarm is triggered"){
+        input "alarms", "capability.alarm", title: "Which Alarm(s) to trigger when ADT alarm goes off", multiple: true, required: false
+        input "alarmtype", "number", title: "What type of alarm do you want to trigger", required: false, defaultValue: 3, options: [
+			1:"Siren",
+			2:"Strobe",
+			3:"Both",
+		]
+        paragraph "Valid alarm types are 1= Siren, 2=Strobe, and 3=Both. All other numberical valudes wil be ignored"
+        input "switches", "capability.switch", title: "Flash these lights (optional) when alarm is triggered", multiple: true, required: false
+    }
+    section("Flashing Lights setup (Optional)"){
+		input "onFor", "number", title: "On for (default 5000)", required: false
+		input "offFor", "number", title: "Off for (default 5000)", required: false
+        input "numFlashes", "number", title: "This number of times (default 3)", required: false
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	subscribeToEvents()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	subscribeToEvents()
+}
+
+def subscribeToEvents() {
+        subscribe(location, "alarm", alarmHandler)
+}
+
+def devices = " "
+
+def alarmHandler(evt) {
+
+switch (evt.value)
+	{
+    case "CLEARED":
+    log.debug "Notify got alarm clear event ${evt}"
+    alarms?.off()
+    	break 
+    case "siren":
+    log.debug "siren turned on"
+    	break
+    case "strobe":
+    log.debug "Strobe is turned on"
+    	break
+    case "both":
+    log.debug "Siren and Strobe turned on"
+    	break
+    case "off":
+    log.debug "Siren and Strobe turned off"
+    	break
+    default:
+	log.debug "Notify got alarm event ${evt}"
+    log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
+        log.debug "The event id to be compared is ${evt.value}"  
+		if (settings.water != null) {
+    	def devices = settings.water
+        log.debug "These devices were found ${devices.id} are being reviewed."
+    	devices.findAll { it.id == evt.value } .each { 
+        log.debug "Found device: ID: ${it.id}, Label: ${it.label}, Name: ${it.name}, Is water Event"
+        switch (alarmtype.value)
+        	{
+            	case 1 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on siren"
+                    alarms?.siren()
+                    break
+                case 2 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on strobe"
+                    alarms?.strobe()
+                    break
+                case 3 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on Siren and Strobe"
+                    alarms?.both()
+                    break
+                default:
+					log.debug "Ignoring unexpected alarmtype mode."
+        			log.debug "Alarm type ${alarmtype.value} detected"
+                    break
+                    }
+        }}
+		else if (settings.contact != null) {
+        def devices = settings.contact
+        log.debug "These devices were found ${devices.id} are being reviewed."
+    	devices.findAll { it.id == evt.value } .each { 
+        log.debug "Found device: ID: ${it.id}, Label: ${it.label}, Name: ${it.name}, Is Intruder event Event"
+        switch (alarmtype.value)
+        	{
+            	case 1 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on siren"
+                    alarms?.siren()
+                    break
+                case 2 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on strobe"
+                    alarms?.strobe()
+                    break
+                case 3 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on Siren and Strobe"
+                    alarms?.both()
+                    break
+                default:
+					log.debug "Ignoring unexpected alarmtype mode."
+        			log.debug "Alarm type ${alarmtype.value} detected"
+                    break
+                    }
+        }}
+        else if (settings.motion != null) {
+        def devices = settings.motion
+        log.debug "These devices were found ${devices.id} are being reviewed."
+    	devices.findAll { it.id == evt.value } .each { 
+        log.debug "Found device: ID: ${it.id}, Label: ${it.label}, Name: ${it.name}, Is Intruder event Event"
+        switch (alarmtype.value)
+        	{
+            	case 1 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on siren"
+                    alarms?.siren()
+                    break
+                case 2 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on strobe"
+                    alarms?.strobe()
+                    break
+                case 3 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on Siren and Strobe"
+                    alarms?.both()
+                    break
+                default:
+					log.debug "Ignoring unexpected alarmtype mode."
+        			log.debug "Alarm type ${alarmtype.value} detected"
+                    break
+                    }
+        }}
+        else if (settings.smoke != null) {
+        def devices = settings.smoke
+        log.debug "These devices were found ${devices.id} are being reviewed."
+    	devices.findAll { it.id == evt.value } .each { 
+        log.debug "Found device: ID: ${it.id}, Label: ${it.label}, Name: ${it.name}, Is Fire event Event"
+        switch (alarmtype.value)
+        	{
+            	case 1 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on siren"
+                    alarms?.siren()
+                    break
+                case 2 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on strobe"
+                    alarms?.strobe()
+                    break
+                case 3 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on Siren and Strobe"
+                    alarms?.both()
+                    break
+                default:
+					log.debug "Ignoring unexpected alarmtype mode."
+        			log.debug "Alarm type ${alarmtype.value} detected"
+                    break
+                    }
+		}}
+		flashLights()
+		break
+}
+}
+
+def continueFlashing()
+{
+	unschedule()
+	if (state.alarmActive) {
+		flashLights(10)
+		schedule(util.cronExpression(now() + 10000), "continueFlashing")
+	}
+}
+
+private flashLights() {
+	def doFlash = true
+	def onFor = onFor ?: 5000
+	def offFor = offFor ?: 5000
+	def numFlashes = numFlashes ?: 3
+
+	log.debug "LAST ACTIVATED IS: ${state.lastActivated}"
+	if (state.lastActivated) {
+		def elapsed = now() - state.lastActivated
+		def sequenceTime = (numFlashes + 1) * (onFor + offFor)
+		doFlash = elapsed > sequenceTime
+		log.debug "DO FLASH: $doFlash, ELAPSED: $elapsed, LAST ACTIVATED: ${state.lastActivated}"
+	}
+
+	if (doFlash) {
+		log.debug "FLASHING $numFlashes times"
+		state.lastActivated = now()
+		log.debug "LAST ACTIVATED SET TO: ${state.lastActivated}"
+		def initialActionOn = switches.collect{it.currentSwitch != "on"}
+		def delay = 0L
+		numFlashes.times {
+			log.trace "Switch on after  $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.on(delay: delay)
+				}
+				else {
+					s.off(delay:delay)
+				}
+			}
+			delay += onFor
+			log.trace "Switch off after $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.off(delay: delay)
+				}
+				else {
+					s.on(delay:delay)
+				}
+			}
+			delay += offFor
+		}
+	}
+}
+
