aaa3855b983ef65ba0ec0fe052566570bdc58b3a
CRAIG KING <mavrrick58@gmail.com>
2018-07-19 01:22:52 -0400

    ADT Alert Any Sensor

---
+++ smartapps/mavrrick/adt-alert-any-sensor.src/adt-alert-any-sensor.groovy
@@ -0,0 +1,260 @@
+/**
+ *  ADT Alert Any Sensor
+ *
+ *  Copyright 2018 CRAIG KING
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "ADT Alert Any Sensor",
+    namespace: "Mavrrick",
+    author: "CRAIG KING",
+    description: "Smartthing ADT tools for additional functions ",
+    category: "Safety & Security",
+    parent: "Mavrrick:ADT Tools",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+/* 
+*
+* Initial release v1.0.0
+* Trigger action based on ADT Alarm. This is intial release of child app
+*/
+preferences {
+	    section("Monitor these devices for activity when alarm is armed"){
+		paragraph "What Active alarm mode do you want to monitor for 1= Arm/Stay, 2=Armed/Away. All other numberical valudes wil be ignored"
+        	input "alarmtype2", "number", title: "What type of alarm do you want to trigger", required: false, defaultValue: 1        
+        	input "motion", "capability.motionSensor", title: "Motion Sensor?", required: false , multiple: true
+			input "contact", "capability.contactSensor", title: "Door or Window Sensor?", required: true, multiple: true
+		}
+		section("Select your ADT Smart Panel..."){
+		input "panel", "capability.battery", title: "ADT Panel?", required: true
+	}
+		section("Action to trigger"){
+        	input "alarms", "capability.alarm", title: "Which Alarm(s) to trigger when ADT alarm goes off", multiple: true, required: false
+        paragraph "Valid alarm types are 1= Siren, 2=Strobe, and 3=Both. All other numberical valudes wil be ignored"
+        	input "alarmtype", "number", title: "What type of alarm do you want to trigger", required: false, defaultValue: 3
+        paragraph "Valid Light actions are are 1 = None, 2 = Turn on lights, 3 = Flash Lights and 4 = Both. All other numberical valudes wil be ignored"
+        	input "lightaction", "number", title: "What type of light action do you want to trigger", required: true, defaultValue: 1
+        paragraph "If you choose Light action 4 do not select the same lights in both values"
+        	input "switches2", "capability.switch", title: "Turn these lights on if Light action is set to 2 or 4", multiple: true, required: false
+        	input "switches", "capability.switch", title: "Flash these lights (optional) If Light action is set to 3 or 4", multiple: true, required: false
+    	}
+    	section("Flashing Lights setup (Optional)"){
+		input "onFor", "number", title: "On for (default 5000)", required: false
+		input "offFor", "number", title: "Off for (default 5000)", required: false
+        input "numFlashes", "number", title: "This number of times (default 3)", required: false
+		}
+        section("Via a push notification and/or an SMS message"){
+        input "message", "text", title: "Send this message if activity is detected", required: false
+        }
+        section("Via a push notification and/or an SMS message"){
+			input("recipients", "contact", title: "Send notifications to") {
+			input "phone", "phone", title: "Enter a phone number to get SMS", required: false
+		paragraph "If outside the US please make sure to enter the proper country code"
+			input "pushAndPhone", "enum", title: "Notify me via Push Notification", required: false, options: ["Yes", "No"]
+		}
+	}
+		section("Minimum time between messages (optional, defaults to every message)") {
+			input "frequency", "decimal", title: "Minutes", required: false
+	}
+	}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	subscribeToEvents()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	subscribeToEvents()
+}
+
+def subscribeToEvents() {
+        subscribe(location, "securitySystemStatus", alarmHandler)
+	if (contact) {
+		subscribe(contact, "contact.open", triggerHandler)
+	}
+	if (motion) {
+		subscribe(motion, "motion.active", triggerHandler)
+	}
+}
+
+def devices = " "
+
+def alarmHandler(evt) {
+	if (evt.value == 'disarmed') {
+    log.debug "Alarm switch to disarmed. Turing off siren."
+    	alarms?.off() }
+}
+
+def triggerHandler(evt) {
+       def alarmState = panel.currentSecuritySystemStatus
+         if (alarmState == "armedStay" && alarmtype2 == 1) {
+        log.debug "Current alarm mode: ${alarmState}."
+        alarmAction()
+        }
+        else if (alarmState == "armedAway" && alarmtype2 == 2) {
+        log.debug "Current alarm mode: ${alarmState}."
+        alarmAction()
+        }
+        else
+        log.debug "Current alarm mode: ${alarmState}. Ignoring event"
+    }
+    
+def alarmAction()    
+	{
+/*	log.debug "Notify got alarm event ${evt}"
+    log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
+        log.debug "The event id to be compared is ${evt.value}"     
+		def devices = settings.contact
+        log.debug "These devices were found ${devices.id} are being reviewed."
+    	devices.findAll { it.id == evt.value } .each { 
+        log.debug "Found device: ID: ${it.id}, Label: ${it.label}, Name: ${it.name}, Is water Event" */
+        switch (alarmtype.value)
+        	{
+            	case 1 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on siren"
+                    alarms?.siren()
+                    break
+                case 2 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on strobe"
+                    alarms?.strobe()
+                    break
+                case 3 :
+                	log.debug "Alarm type ${alarmtype.value} detected. Turning on Siren and Strobe"
+                    alarms?.both()
+                    break
+                default:
+					log.debug "Ignoring unexpected alarmtype mode."
+        			log.debug "Alarm type ${alarmtype.value} detected"
+                    break
+                    }
+         switch (lightaction.value)
+        	{
+            	case 1 :
+                	log.debug "Light action ${lightaction.value} detected. No Light Action"                    
+                    break
+                case 2 :
+                	log.debug "Light action ${lightaction.value} detected. Turning on selected lights"
+                    switches2?.on()
+                    break
+                case 3 :
+                	log.debug "Light Action ${lightaction.value} detected. Flashing Selected lights"                    
+                    flashLights()
+                    break
+                case 4 :
+                	log.debug "Light Action ${lightaction.value} detected. Flash and turning on selected lights"
+                    switches2?.on()
+                    flashLights()
+                    break
+                default:
+					log.debug "Ignoring unexpected Light Action type."
+        			log.debug "Light Action ${lightaction.value} detected"
+                    break
+			} 		
+sendnotification()
+}
+
+
+def continueFlashing()
+{
+	unschedule()
+	if (state.alarmActive) {
+		flashLights(10)
+		schedule(util.cronExpression(now() + 10000), "continueFlashing")
+	}
+}
+
+private flashLights() {
+	def doFlash = true
+	def onFor = onFor ?: 5000
+	def offFor = offFor ?: 5000
+	def numFlashes = numFlashes ?: 3
+
+	log.debug "LAST ACTIVATED IS: ${state.lastActivated}"
+	if (state.lastActivated) {
+		def elapsed = now() - state.lastActivated
+		def sequenceTime = (numFlashes + 1) * (onFor + offFor)
+		doFlash = elapsed > sequenceTime
+		log.debug "DO FLASH: $doFlash, ELAPSED: $elapsed, LAST ACTIVATED: ${state.lastActivated}"
+	}
+
+	if (doFlash) {
+		log.debug "FLASHING $numFlashes times"
+		state.lastActivated = now()
+		log.debug "LAST ACTIVATED SET TO: ${state.lastActivated}"
+		def initialActionOn = switches.collect{it.currentSwitch != "on"}
+		def delay = 0L
+		numFlashes.times {
+			log.trace "Switch on after  $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.on(delay: delay)
+				}
+				else {
+					s.off(delay:delay)
+				}
+			}
+			delay += onFor
+			log.trace "Switch off after $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.off(delay: delay)
+				}
+				else {
+					s.on(delay:delay)
+				}
+			}
+			delay += offFor
+		}
+	}
+}
+
+def sendnotification() {
+def msg = message
+        if ( msg == null ) {
+        	log.debug "Message not configured. Skipping notification"
+            }
+        else {
+        log.debug "Alarm Notification., '$msg'"
+/*        log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'" */
+
+	Map options = [:]	
+
+	if (location.contactBookEnabled) {
+		sendNotificationToContacts(msg, recipients, options)
+	} else {
+		if (phone) {
+			options.phone = phone
+			if (pushAndPhone != 'No') {
+				log.debug 'Sending push and SMS'
+				options.method = 'both'
+			} else {
+				log.debug 'Sending SMS'
+				options.method = 'phone'
+			}
+		} else if (pushAndPhone != 'No') {
+			log.debug 'Sending push'
+			options.method = 'push'
+		} else {
+			log.debug 'Sending nothing'
+			options.method = 'none'
+		}
+		sendNotification(msg, options)
+	}
+	if (frequency) {
+		state[evt.deviceId] = now()
+	}
+}
+}
