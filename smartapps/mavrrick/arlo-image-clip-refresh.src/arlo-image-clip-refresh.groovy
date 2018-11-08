41d9f870f1b8ff7b31666b681db4256324c23921
CRAIG KING <mavrrick58@gmail.com>
2018-11-08 15:16:20 -0500

    Addition of "Arlo Image/clip Refresh" app for Arlo integration.

---
+++ smartapps/mavrrick/arlo-image-clip-refresh.src/arlo-image-clip-refresh.groovy
@@ -0,0 +1,62 @@
+/**
+ *  Arlo Image/Clip Refresh
+ *
+ *  Copyright 2018 Mavrrick
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Arlo Image/Clip Refresh",
+    namespace: "Mavrrick",
+    author: "Mavrrick",
+    description: "Create a 5 second clip to refresh image tile.",
+    category: "My Apps",
+	iconUrl: "https://storage.googleapis.com/arlopilot/arlo-small.png",
+	iconX2Url: "https://storage.googleapis.com/arlopilot/arlo-med.png",
+	iconX3Url: "https://storage.googleapis.com/arlopilot/arlo-large.png"
+    )
+
+import groovy.time.TimeCategory 
+
+preferences {
+	section("Setup") {
+		input "cameras", "capability.videoCapture", multiple: true
+        input name: "clipLength", type: "number", title: "Clip Length", description: "Please enter the length of each recording", required: true, range: "5..120"
+        input name: "frequency", type: "number", title: "Frequency to refresh cameras in min", description: "Please specify how often you would like to refresh cameras", required: true
+        }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	schedule("0 0 0/${frequency} * * ?", arloRefresh)
+}
+
+def arloRefresh() {	
+	log.debug "$evt.name: $evt.value"
+	log.debug "Refreshing cameras with ${clipLength} second capture"
+    Date start = new Date()
+    Date end = new Date()
+    use( TimeCategory ) {
+    	end = start + clipLength.seconds
+ 	}
+    log.debug "Capturing..."
+    cameras.capture(start, start, end)
+}
